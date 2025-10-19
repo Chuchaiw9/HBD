5a19d1e77aa3ef3e89e5b47faa5ae8fc6aca8f93
Chuchaiw9 <chuchai225511@gmail.com>
2025-10-19 12:41:10 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,35 @@
 
+let currentPage = 1;
+const totalPages = 3;
+
+function showPage(page) {
+    for (let i = 1; i <= totalPages; i++) {
+        const p = document.getElementById(`page${i}`);
+        p.classList.add('hidden');
+    }
+    document.getElementById(`page${page}`).classList.remove('hidden');
+}
+
+function nextPage() {
+    if (currentPage < totalPages) {
+        currentPage++;
+        showPage(currentPage);
+    }
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        showPage(currentPage);
+    }
+}
+
+function addMessage() {
+    const msg = document.getElementById('message').value;
+    alert(`ข้อความของคุณ: ${msg}`);
+}
+
+window.onload = function() {
+    showPage(currentPage);
+    document.getElementById('music').play();
+};
